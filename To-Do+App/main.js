ef94afc95cfa0d049cea680379918cec51e3817b
John Terry <92858448+Johnterrie@users.noreply.github.com>
2023-04-28 18:06:12 +0100

    Add files via upload

---
+++ To-Do+App/main.js
@@ -0,0 +1,67 @@
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const todos = document.getElementById("todos");
+
+const todosUl = JSON.parse(localStorage.getItem("todos"));
+
+if (todosUl) {
+    todosUl.forEach(todo => {
+        addTodo(todo)
+    })
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  addTodo()
+});
+
+function addTodo(todo) {
+    let todoText = input.value;
+
+    if(todo) {
+        todoText = todo.text
+    }
+
+    if (todoText) {
+    const todoEl = document.createElement("li"); 
+
+    if(todo && todo.completed) {
+        todoEl.classList.add("completed");
+    }
+
+    todoEl.innerHTML = todoText;
+
+    todoEl.addEventListener("click", (e) => {
+        todoEl.classList.toggle("completed");
+
+        updateLS()
+    });
+
+    todoEl.addEventListener('contextmenu', (e) => {
+        e.preventDefault();
+
+        todoEl.remove();
+    })
+
+    todos.appendChild(todoEl)
+     input.value = "";
+
+     updateLS()
+  }
+}
+
+function updateLS() {
+    const todosEl = document.querySelectorAll("li");
+
+    const todos = [];
+
+    todosEl.forEach(todoEl => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains("completed")
+        })
+    })
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
